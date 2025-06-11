013e8c53c0de7b195788814af2e3c12ab9b05d38
Arianemld <at>
2025-06-11 12:01:21 +0200

    Mise à jour du test pour la fonctionnalité 'New Feature Added!'

--- test/app.test.js
+++ test/app.test.js
@@ -2,8 +2,8 @@ const request = require('supertest');
 const app = require('../index');  // Importer l'app, mais le serveur ne sera pas démarré
 
 describe('Test /new-feature route', () => {
-  it('should return "New Feature!"', async () => {
+  it('should return "New Feature Added!"', async () => {  // Mise à jour du message attendu
     const response = await request(app).get('/new-feature');
-    expect(response.text).toBe('New Feature!');
+    expect(response.text).toBe('New Feature Added!');
   });
 });
